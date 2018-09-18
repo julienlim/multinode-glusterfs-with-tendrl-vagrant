368a9ea2016c3e31a4017703a130a7c150da6ef3
Ju Lim (Red Hat) <julim@redhat.com>
2018-09-18 13:53:03 -0400

    Update bootstrap_upstream.sh
    Automate creation of inventory file

--- bootstrap_upstream.sh
+++ bootstrap_upstream.sh
@@ -32,6 +32,37 @@ then
   #cd /opt
   #git clone https://github.com/Tendrl/tendrl-ansible.git
   yum install tendrl-ansible -y
+  
+  # create inventory file for tendrl-ansible
+  cd /usr/share/doc/tendrl-ansible-*
+
+  cat > ./inventory <<EOL
+
+[gluster_servers]
+node1
+node2
+node3
+[tendrl_server]
+node0
+[all:vars]
+
+# Mandatory variables. In this example, 192.0.2.1 is ip address of tendrl
+# server, tendrl.example.com is a hostname of tendrl server and
+# tendrl.example.com hostname is translated to 192.0.2.1 ip address.
+etcd_ip_address=172.28.128.3
+etcd_fqdn=172.28.128.3
+graphite_fqdn=172.28.128.3
+configure_firewalld_for_tendrl=false
+
+# when direct ssh login of root user is not allowed and you are connecting via
+# non-root cloud-user account, which can leverage sudo to run any command as
+# root without any password
+#ansible_become=yes
+#ansible_user=cloud-user
+
+EOL
+
+# Storage Nodes  
 else
   # install gluster 3.12.1 and gstatus on storage nodes
   sudo yum install centos-release-gluster -y
